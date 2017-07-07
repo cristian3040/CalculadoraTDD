fc0af3b34ba4c98d86c392dbab35480034524fca
cristian3040 <cristian3040@gmail.com>
2017-07-07 13:51:53 +0100

    Inicio testRealizarOperacion

--- src/Calculadora/OperacionBundle/Tests/Util/CalculadoraTest.php
+++ src/Calculadora/OperacionBundle/Tests/Util/CalculadoraTest.php
@@ -27,8 +27,8 @@ class CalculadoraTest extends \PHPUnit_Framework_TestCase
 
 	public function testResta()
 	{
-		$resultado = $this->calculadora->hacerResta(5,3);
-		$this->assertEquals($resultado, 2);
+		$resultado = $this->calculadora->hacerResta(100,3);
+		$this->assertEquals($resultado, 97);
 	}
 
 	public function testRestaConNegativos()
@@ -39,8 +39,52 @@ class CalculadoraTest extends \PHPUnit_Framework_TestCase
 
 	public function testComprobarLimites()
 	{
-		$calculadora = new Calculadora(-100,100);
-		$this->assertEquals($calculadora->getMaximo(), 100);
-		$this->assertEquals($calculadora->getMinimo(), -100);
+		$this->assertEquals($this->calculadora->getMaximo(), 100);
+		$this->assertEquals($this->calculadora->getMinimo(), -100);
 	}
+
+	public function testComprobarRangosOperandos()
+	{
+		$resultado = $this->calculadora->comprobarRangosOperandos(200,5);
+		$this->assertEquals($resultado, false);
+		$resultado = $this->calculadora->comprobarRangosOperandos(5,-200);
+		$this->assertEquals($resultado, false);
+		$resultado = $this->calculadora->comprobarRangosOperandos(5,-5);
+		$this->assertEquals($resultado, true);
+	}
+
+	public function testComprobarRangoResultado()
+	{
+		$resultado = $this->calculadora->comprobarRangoResultado(110);
+		$this->assertEquals($resultado, false);
+	}
+
+	public function testSepararCadena()
+	{
+		$cadena = $this->calculadora->separarCadena("2 + 2 - 6");
+		$this->assertCount(5, $cadena);
+	    $this->assertEquals($cadena[0],"2");
+	    $this->assertEquals($cadena[1],"+");
+	    $this->assertEquals($cadena[2],"2");
+	    $this->assertEquals($cadena[3],"-");
+	    $this->assertEquals($cadena[4],"6");
+	}
+
+	public function testValidarOperacion()
+	{
+		$cadena = $this->calculadora->validarOperacion("-2 + 2 - 65656");
+		$this->assertEquals($cadena, true);
+		$cadena = $this->calculadora->validarOperacion("**2 + 5");
+		$this->assertEquals($cadena, false);
+	}
+
+	/*public function testRealizarOperacion()
+	{
+		$cadena = array("1", "+", "1", "+", "1");
+		$operacion = $this->calculadora->realizarOperacion($cadena);
+		$this->assertEquals($operacion, 6);
+		
+	}*/
+
+
 }
--- src/Calculadora/OperacionBundle/Util/Calculadora.php
+++ src/Calculadora/OperacionBundle/Util/Calculadora.php
@@ -6,18 +6,56 @@ class Calculadora
 	private $minimo = -100;
 	private $maximo = 100;
 
+	public function validarOperacion($cadena)
+	{
+		$patron = "/^-{0,1}\d+((\s+)[+\/*-](\s+)-{0,1}\d+)+$/";
+		if(preg_match($patron, $cadena))return true;
+	}
+
 	public function hacerSuma($operando1, $operando2)
 	{
-		return $operando1 + $operando2;
-		
+		$resultado = $operando1 - $operando2;
+		$valida = $this->comprobarRangosOperandos($operando1, $operando2);
+		if($valida) return $operando1 + $operando2;
 	}
 
 	public function hacerResta($operando1, $operando2)
 	{
-		return $operando1 - $operando2;
+		$resultado = $operando1 - $operando2;
+		$valida = $this->comprobarRangosOperandos($operando1, $operando2);
+		if($valida) return $operando1 - $operando2;
 	}
 
-    /**
+	public function comprobarRangosOperandos($operando1, $operando2){
+		if($operando1 < $this->getMinimo() || $operando1 > $this->getMaximo() || $operando2 < $this->getMinimo() || $operando2 > $this->getMaximo()) return false;
+		return true;
+	}
+
+	public function comprobarRangoResultado($resultado){
+		if($resultado < $this->getMinimo() || $resultado > $this->getMaximo()) return false;
+		throw new Exception("Error Processing Request", 1);
+		
+		return true;
+	}
+
+	public function separarCadena($cadena)
+	{
+		$tokens = explode(" ", $cadena);
+		return $tokens;
+	}
+
+	/*public function realizarOperacion($cadena)
+	{
+		$operacion = 0;
+		for ($i=1; $i < count($cadena)-1; $i = $i + 2) { 
+			$op = $this->hacerSuma($cadena[$i-1],$cadena[$i+1]);
+			$operacion += $op;
+			
+		}
+		return $operacion;
+	}*/
+
+	/**
      * @return mixed
      */
     public function getMinimo()
